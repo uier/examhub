a137ea05643b32079d4555f9afba9feb988b9c87
Uier <ap9940506@gmail.com>
2021-05-25 04:15:55 +0800

    refactor(server/logger): refine logger's log format

--- server/src/middlewares/logger.ts
+++ server/src/middlewares/logger.ts
@@ -1,41 +1,31 @@
 import winston from 'winston';
 import expressWinston from 'express-winston';
 
+const logFormat = winston.format.combine(
+  winston.format.prettyPrint(),
+  winston.format.timestamp({
+    format: 'YYYY-MM-DD HH:mm:ss.SSS',
+  }),
+  winston.format.printf(({
+    timestamp, level, message, ...metadata
+  }) => {
+    let msg = `${timestamp} [${level}]: ${message} `;
+    if (metadata) msg += JSON.stringify(metadata, null, 2);
+    return msg;
+  }),
+);
+
 export const requestLogger = expressWinston.logger({
   transports: [
     new winston.transports.File({ filename: 'request.log' }),
   ],
   msg: 'HTTP {{ req.method }} {{req.url}} {{ res.statusCode }}',
-  format: winston.format.combine(
-    winston.format.prettyPrint(),
-    winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss.SSS',
-    }),
-    winston.format.printf(({
-      timestamp, level, message, ...metadata
-    }) => {
-      let msg = `${timestamp} [${level}]: ${message} `;
-      if (metadata) msg += JSON.stringify(metadata);
-      return msg;
-    }),
-  ),
+  format: logFormat,
 });
 
 export const errorLogger = expressWinston.errorLogger({
   transports: [
     new winston.transports.File({ filename: 'error.log' }),
   ],
-  format: winston.format.combine(
-    winston.format.prettyPrint(),
-    winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss.SSS',
-    }),
-    winston.format.printf(({
-      timestamp, level, message, ...metadata
-    }) => {
-      let msg = `${timestamp} [${level}]: ${message} `;
-      if (metadata) msg += JSON.stringify(metadata, null, 2);
-      return msg;
-    }),
-  ),
+  format: logFormat,
 });
