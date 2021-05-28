097d2d95fad755e63949bf2b57c6c08865b47b97
Uier <ap9940506@gmail.com>
2021-05-28 22:39:30 +0800

    refactor(dotenv): preload env instead of load in app code

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,8 +3,6 @@ import { requestLogger, errorLogger } from './middlewares/logger';
 import { db } from './models';
 import router from './routes';
 
-require('dotenv').config();
-
 const app = express();
 const port = 3000;
 
