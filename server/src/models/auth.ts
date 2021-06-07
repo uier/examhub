c1bc707b56feab25b43e3f2c795ba9c2a9dc9e0d
Uier <ap9940506@gmail.com>
2021-06-08 03:12:40 +0800

    chore(mysql): since using mysql2, use promise wrapper

--- server/src/models/auth.ts
+++ server/src/models/auth.ts
@@ -1,22 +1,8 @@
 import { pool } from '.';
 
-const getAllUsers = () => new Promise((resolve, reject) => {
-  pool.query('SELECT * FROM user', (error, results) => {
-    if (error) {
-      reject(error);
-    }
-    resolve(results);
-  });
-});
+const getAllUsers = () => pool.promise().query('SELECT * FROM user');
 
-const addNewUser = (newUser: any) => new Promise((resolve, reject) => {
-  pool.query('INSERT INTO user SET ?', newUser, (error, results) => {
-    if (error) {
-      reject(error);
-    }
-    resolve(results);
-  });
-});
+const addNewUser = (newUser: any) => pool.promise().query('INSERT INTO user SET ?', newUser);
 
 export default {
   getAllUsers,
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -5,26 +5,32 @@ import hash from '../utils/hash';
 
 const router: Router = Router();
 
-router.get('/', (req, res, next) => {
-  db.auth.getAllUsers()
-    .then((results) => res.json(results))
-    .catch(next);
+router.get('/', async (req, res, next) => {
+  try {
+    const [rows] = await db.auth.getAllUsers();
+    res.json(rows);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/', (req, res, next) => {
-  const {
-    userId, name, email, password,
-  } = req.body;
-  const newUser = {
-    user_id: hash(process.env.SECRET, userId),
-    name,
-    email,
-    create_time: dayjs().format(),
-    password: hash(process.env.SECRET, password),
-  };
-  db.auth.addNewUser(newUser)
-    .then(() => res.status(200).send('Success.'))
-    .catch(next);
+router.post('/', async (req, res, next) => {
+  try {
+    const {
+      userId, name, email, password,
+    } = req.body;
+    const newUser = {
+      user_id: hash(process.env.SECRET, userId),
+      name,
+      email,
+      create_time: dayjs().format(),
+      password: hash(process.env.SECRET, password),
+    };
+    await db.auth.addNewUser(newUser);
+    res.status(200).send('Success.');
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
