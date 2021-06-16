7a7ce59432d5ea36f89549b2cfa4ebce85647287
Uier <ap9940506@gmail.com>
2021-06-16 17:29:16 +0800

    feat(client): build announcement page and business logic

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,17 +1,24 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosPromise } from 'axios';
 
 const agent = axios.create({ baseURL: '/api' });
 
+const Announcement = {
+  get: (annId: number): AxiosPromise<Announcement.Info> => agent.get(`/announcements/${annId}`),
+  getList: (): AxiosPromise<Announcement.List> => agent.get('/announcements'),
+  create: (body: Announcement.CreateBody): AxiosPromise<number> => agent.post('/announcements', body),
+};
+
 const Auth = {
-  logout: (): Promise<AxiosResponse> => agent.get('/auth/logout'),
+  logout: (): AxiosPromise => agent.get('/auth/logout'),
 };
 
 const Users = {
-  getUsers: (): Promise<AxiosResponse<User.Info[]>> => agent.get('/users'),
-  getMe: (): Promise<AxiosResponse<User.Info>> => agent.get('/users/me'),
+  getUsers: (): AxiosPromise<User.Info[]> => agent.get('/users'),
+  getMe: (): AxiosPromise<User.Info> => agent.get('/users/me'),
 };
 
 export default {
+  Announcement,
   Auth,
   Users,
 };
