67641014531b13e2835e2c1f8439019149e6e96d
Uier <ap9940506@gmail.com>
2021-06-19 15:04:01 +0800

    style(exam): formatting

--- server/src/models/comment-area.ts
+++ server/src/models/comment-area.ts
@@ -1,12 +1,12 @@
+import { OkPacket } from 'mysql2';
 import { pool } from '.';
-import console from 'console';
 
 const addcommentArea = async () => {
   const sql = 'INSERT INTO `comment_area` VALUES()';
-  const [result] = await pool.promise().query(sql);
-  return Number(result["insertId"]);
-}
+  const [result] = await pool.promise().query(sql) as unknown as OkPacket[];
+  return Number(result.insertId);
+};
 
 export default {
-  addcommentArea
+  addcommentArea,
 };
--- server/src/models/exam.ts
+++ server/src/models/exam.ts
@@ -1,6 +1,5 @@
 import dayjs from 'dayjs';
-import { pool } from '.';
-import db from '../models';
+import db, { pool } from '.';
 
 const getAllExams = () => {
   const sql = 'SELECT `docId`, `courseId`, `year`, `semester`, `title`, `description`, `userId`, `createTime`, `lastUpdateTime`, `upvote`, `downvote`, `folderPath` FROM `document`';
@@ -10,11 +9,18 @@ const getAllExams = () => {
 const getExamById = (docId: number) => {
   const sql = 'SELECT `docId`, `courseId`, `year`, `semester`, `title`, `description`, `userId`, `createTime`, `lastUpdateTime`, `upvote`, `downvote`, `folderPath` FROM `document` WHERE `docId` = ?';
   return pool.promise().query(sql, [docId]);
-}
+};
 
-const addExam = async ( userId: number, courseId: number, year: number, semester: number, title: string, description: string) => {
+const addExam = async (
+  userId: number,
+  courseId: number,
+  year: number,
+  semester: number,
+  title: string,
+  description: string,
+) => {
   const docId = await db.commentArea.addcommentArea();
-  const folderPath = "i am path, hi";
+  const folderPath = 'i am path, hi';
   const newExam = {
     docId,
     userId,
@@ -29,22 +35,22 @@ const addExam = async ( userId: number, courseId: number, year: number, semester
   };
   const sql = 'INSERT INTO `document` SET ?';
   return pool.promise().query(sql, newExam);
-}
+};
 
 const delExamById = async (docId: number) => {
   const sql = 'DELETE FROM `document` WHERE `docId` = ?';
   return pool.promise().query(sql, [docId]);
-}
+};
 
 const checkCourseIdExist = (courseId: number) => {
   const sql = 'SELECT `docId` FROM `document` WHERE `courseId` = ?';
   return pool.promise().query(sql, [courseId]);
-}
+};
 
 const getExamByCourseId = (courseId: number) => {
   const sql = 'SELECT `docId` FROM `document` WHERE `courseId` = ?';
   return pool.promise().query(sql, [courseId]);
-}
+};
 
 export default {
   getAllExams,
@@ -52,5 +58,5 @@ export default {
   addExam,
   delExamById,
   checkCourseIdExist,
-  getExamByCourseId
+  getExamByCourseId,
 };
