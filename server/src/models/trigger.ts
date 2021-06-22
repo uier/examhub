ca6319c23a2328126f0e6d42c0c65a0fc25b76aa
Uier <ap9940506@gmail.com>
2021-06-23 02:48:17 +0800

    style(trigger): formatting

--- server/src/models/trigger.ts
+++ server/src/models/trigger.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-multi-str */
 export default [
   'DROP TRIGGER IF EXISTS `insertVote`; \
   CREATE TRIGGER `insertVote` AFTER INSERT \
@@ -7,8 +8,7 @@ export default [
       IF NEW.`score` = 1 THEN \
         UPDATE user SET `contribution` = `contribution`+1 WHERE `user`.`userId` in (SELECT `userId` FROM `document` WHERE `docId` = NEW.`docId`); \
       END IF; \
-    END'
-  ,
+    END',
   'DROP TRIGGER IF EXISTS deleteVote; \
   CREATE TRIGGER deleteVote AFTER DELETE \
   ON vote FOR EACH ROW \
@@ -17,8 +17,7 @@ export default [
       IF OLD.score = 1 THEN \
         UPDATE user SET contribution = contribution - 1 WHERE user.userId = (SELECT userId FROM document WHERE docId = OLD.docId); \
       END IF; \
-    END'
-  ,
+    END',
   'DROP TRIGGER IF EXISTS updateVote; \
   CREATE TRIGGER updateVote BEFORE UPDATE \
   ON vote FOR EACH ROW \
@@ -28,35 +27,30 @@ export default [
       ELSEIF NEW.score = -1 THEN \
         UPDATE user SET contribution = contribution - 1 WHERE user.userId in (SELECT userId FROM document WHERE docId = NEW.docId); \
       END IF; \
-    END'
-  ,
+    END',
   'DROP TRIGGER IF EXISTS addComment; \
   CREATE TRIGGER addComment BEFORE INSERT \
   ON comment FOR EACH ROW \
     BEGIN \
       UPDATE user SET contribution = contribution + 1 \
       WHERE NEW.replyId in (SELECT docId as replyId FROM document) and NEW.userId not in (SELECT userId FROM comment WHERE comment.replyId = NEW.replyId); \
-    END'
-  ,
+    END',
   'DROP TRIGGER IF EXISTS deleteComment; \
   CREATE TRIGGER deleteComment AFTER DELETE \
   ON comment FOR EACH ROW \
     BEGIN \
       UPDATE user SET contribution = contribution - 1 WHERE user.userId = OLD.userId and OLD.userId NOT IN (SELECT userId FROM comment WHERE comment.docId = OLD.docId); \
-    END'
-  ,
+    END',
   'DROP TRIGGER IF EXISTS addDocument; \
   CREATE TRIGGER addDocument AFTER INSERT \
   ON document FOR EACH ROW \
     BEGIN \
       UPDATE user SET contribution = contribution + 5 WHERE user.userId = NEW.userId; \
-    END'
-  ,
+    END',
   'DROP TRIGGER IF EXISTS delDocument; \
   CREATE TRIGGER delDocument AFTER DELETE \
   ON document FOR EACH ROW \
     BEGIN \
       UPDATE user SET contribution = contribution - 5 WHERE user.userId = OLD.userId; \
-    END'
-  ,
+    END',
 ];
