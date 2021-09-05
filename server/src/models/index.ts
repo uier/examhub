22ed4751ca22d862d956aa9bcb26124e5a8a2b6e
TzuWei <ap9940506@gmail.com>
2021-09-05 23:26:08 +0800

    build: chg caddy port, fix db env & server dockerfile

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -11,20 +11,34 @@ import vote from './vote';
 
 export const pool = mysql.createPool({
   connectionLimit: 10,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWD,
-  database: process.env.DB_NAME,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
 });
+
+const attemptConnection = (sql: string) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.log(`Connection failed: ${err}, Retrying connection in 2 seconds...`)
+      setTimeout(() => attemptConnection(sql), 2000);
+    } else {
+      connection.query(sql, (error) => {
+        connection.release();
+        if (error) {
+          console.log(`Initialize schema (${sql.slice(0, 40)}) failed: ${error}`);
+        }
+      });
+    }
+  })
+}
 
 export default {
   // initialize db schema with prepared sql queries
   initDB: () => {
     schema.forEach((s) => {
-      pool.query(s, (error) => {
-        if (error) throw error;
-      });
+      attemptConnection(s)
     });
   },
   // export the models below
